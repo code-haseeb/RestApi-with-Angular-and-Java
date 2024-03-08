7785dc3cb5f86e48eb630b1f09f4d63b88cc23c3
code-haseeb <abdullhaseebonline@gmail.com>
2024-03-08 13:17:59 +0500

    add custom types

---
+++ types/canvas.ts
@@ -0,0 +1,19 @@
+
+export type Color = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+export type Camera = {
+  x: number;
+  y: number;
+};
+
+export enum LayerType {
+  Rectangle,
+  Ellipse,
+  Path,
+  Text,
+  Note,
+};
