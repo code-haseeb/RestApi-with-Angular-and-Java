581c21a6d222b62bdd202ccc82dd87fb4ead6e99
code-haseeb <abdullhaseebonline@gmail.com>
2024-03-08 15:13:30 +0500

    convex & clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
-
+import {UserButton} from "@clerk/nextjs"
 export default function Home() {
   return (
-<main>Hello   </main>
+    <main>authenticated users only
+
+    <UserButton/></main>
   );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://noted-lamb-97.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
