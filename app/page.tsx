898699c649ee67c058f88c8603fb529ac5be9afc
code-haseeb <abdullhaseebonline@gmail.com>
2024-03-08 16:28:17 +0500

    favorites page ready

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import {UserButton} from "@clerk/nextjs"
-export default function Home() {
-  return (
-    <main>authenticated users only
-
-    <UserButton/></main>
-  );
-}
